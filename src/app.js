0e2135f9e497192ee76710068e25f0ed8b5701e3
Laluirgi <laluirginabilfarhan@gmail.com>
2026-05-25 22:51:29 +0700

    Update app.js
    Update app.js - Testing CI Pipeline

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
 console.log("Sistem Otomatisasi Pengingat Tugas");
 console.log("Rule-Based Engine berjalan...");
 console.log("Siap mengingatkan tugas sesuai aturan");
+
+console.log("Update baru untuk testing CI/CD");
